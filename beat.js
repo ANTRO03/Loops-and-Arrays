17fd99842fbe8676e7f2ef0435469459c35eb81d
Tristan Ramos <tristanramos543@gmail.com>
2023-02-12 13:22:09 -0500

    Some message here

---
+++ beat.js
@@ -0,0 +1,62 @@
+
+for (var i = 1000; i > 0; i -= 2) {
+  console.log(i);
+}
+
+console.log(i);
+
+
+
+for (var i = 0; i < 10000; i += 2) {
+   
+}
+
+
+if(i) {
+ alert("A quarter of the way there!")
+}
+
+if(i) {
+   alert("Halfway there!")
+}
+if(i){
+   alert("The loop is done!")
+}
+const firstShow = "The Mandalorian"
+const secondShow ="Jojo's Bizzare Adventure"
+const thirdShow = "The Boys"
+const fourthShow ="Andor"
+const fifthShow ="Demon Slayer"
+
+var ourArray = ["My #1 favorite tv show is" + " " + firstShow];
+console.log(ourArray)
+
+for (var ourArray = "My #1 favorite tv show is"; i > firstShow; i += 1) {
+    console.log(i);
+}
+var ourArray = ["My #2 favorite tv show is" + " " + secondShow];
+console.log(ourArray)
+
+for (var ourArray = "My #2 favorite tv show is"; i > secondShow; i += 1) {
+    console.log(i);
+}
+var ourArray = ["My #3 favorite tv show is" + " " + thirdShow];
+console.log(ourArray)
+
+for (var ourArray = "My #3 favorite tv show is"; i > thirdShow; i += 1) {
+    console.log(i);
+}
+
+var ourArray = ["My #4 favorite tv show is" + " " + fourthShow];
+console.log(ourArray)
+
+for (var ourArray = "My #4 favorite tv show is"; i > fourthShow; i += 1) {
+    console.log(i);
+}
+
+var ourArray = ["My #5 favorite tv show is" + " " + fifthShow];
+console.log(ourArray)
+
+for (var ourArray = "My #5 favorite tv show is"; i > fifthShow; i += 1) {
+    console.log(i);
+}
